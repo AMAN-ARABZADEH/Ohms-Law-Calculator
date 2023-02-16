c73dbdf9da3ed479a4f8ba83f1c5961f4467ca58
Aman Arab <aman.arabzadeh98@gmail.com>
2023-02-17 00:32:05 +0100

    This is first commit

--- main.js
+++ main.js
@@ -3,71 +3,124 @@
 // Date: 2023-02-16
 // I built this calculator for my course in embedded systems at University.
 
-const btn = document.getElementById("btn");
-const resetBtn = document.getElementById("reset");
-const result = document.getElementById("result");
-const resultsheading = document.getElementById("results");
+document.addEventListener("DOMContentLoaded", function () {
+  const btn = document.getElementById("btn");
+  const resetBtn = document.getElementById("reset");
+  const result = document.getElementById("result");
+  const resultsheading = document.getElementById("results");
 
-function styleResult() {
-  result.style.borderBottom = "1px solid black";
-  result.style.width = "200px";
-  resultsheading.innerHTML = "Result";
-  result.style.color = "black";
-}
+  // Listen button
+  btn.addEventListener("click", function () {
+    // Get the user inputs value
+    const voltage = document.querySelector("#voltage").value;
+    const current = document.querySelector("#current").value;
+    const resistance = document.querySelector("#resistance").value;
 
-function negativeValueChecker(voltage, current, resistance) {
-  if (voltage <= 0 || current <= 0 || resistance <= 0) {
-    return false;
+    // Convert the input units to standard units
+    const voltageUnits = document.querySelector("#voltage-units").value;
+    const currentUnits = document.querySelector("#current-units").value;
+    const resistanceUnits = document.querySelector("#resistance-units").value;
+
+    // Take the input value unit check which option is true
+    let voltageInV, voltValue;
+    if (voltageUnits === "voltage") {
+      voltValue = voltageUnits;
+      voltageInV = parseFloat(voltage);
+    } else if (voltageUnits === "kilovolts") {
+      voltageInV = parseFloat(voltage) * 1000;
+    } else if (voltageUnits === "megavolts") {
+      voltageInV = parseFloat(voltage) * 1000000;
+    }
+    let currentInA;
+
+    if (currentUnits === "amps") {
+      currentInA = parseFloat(current);
+    } else if (currentUnits === "kiloamps") {
+      currentInA = parseFloat(current) * 1000;
+    } else if (currentUnits === "megaamps") {
+      currentInA = parseFloat(current) * 1000000;
+    }
+
+    let resistanceInOhms;
+    if (resistanceUnits === "ohms") {
+      resistanceInOhms = parseFloat(resistance);
+    } else if (resistanceUnits === "kiloohms") {
+      resistanceInOhms = parseFloat(resistance) * 1000;
+      console.log(resistanceInOhms.value);
+    } else if (resistanceUnits === "megaohms") {
+      resistanceInOhms = parseFloat(resistance) * 1000000;
+    }
+
+    if (!isNaN(voltageInV) && !isNaN(currentInA) && isNaN(resistanceInOhms)) {
+      if (!negativeValueChecker(voltageInV, currentInA, resistanceInOhms)) {
+        result.innerHTML = "The input values have to be greater than zero!";
+        result.style.color = "red";
+      } else {
+        const res = voltageInV / currentInA;
+        result.innerHTML =
+          "Resistance: " + res.toFixed(6) + " " + resistanceUnits;
+        styleResult();
+      }
+    } else if (
+      !isNaN(voltageInV) &&
+      isNaN(currentInA) &&
+      !isNaN(resistanceInOhms)
+    ) {
+      if (!negativeValueChecker(voltageInV, currentInA, resistanceInOhms)) {
+        result.innerHTML = "The input values have to be greater than zero!";
+        result.style.color = "red";
+      } else {
+        const curr = voltageInV / resistanceInOhms;
+        result.innerHTML = "Current: " + curr.toFixed(6) + " " + currentUnits;
+        styleResult();
+      }
+    } else if (
+      isNaN(voltageInV) &&
+      !isNaN(currentInA) &&
+      !isNaN(resistanceInOhms)
+    ) {
+      if (!negativeValueChecker(voltageInV, currentInA, resistanceInOhms)) {
+        result.innerHTML = "The input values have to be greater than zero!";
+        result.style.color = "red";
+      } else {
+        const volt = currentInA * resistanceInOhms;
+        result.innerHTML = "Voltage: " + volt.toFixed(6) + " " + voltageUnits;
+        styleResult();
+      }
+    } else {
+      result.innerHTML = "Please enter two values to calculate.";
+      result.style.color = "red";
+    }
+  });
+
+  resetBtn.addEventListener("click", function () {
+    document.getElementById("voltage").value = "";
+    document.getElementById("current").value = "";
+    document.getElementById("resistance").value = "";
+    result.innerHTML = "";
+    resultsheading.innerHTML = "";
+  });
+
+  function styleResult() {
+    result.style.width = "200px";
+    resultsheading.innerHTML = "Result";
+    result.style.color = "black";
   }
-  return true;
-}
 
-btn.addEventListener("click", function () {
-  const voltage = parseFloat(document.getElementById("voltage").value);
-  const current = parseFloat(document.getElementById("current").value);
-  const resistance = parseFloat(document.getElementById("resistance").value);
-
-  if (!isNaN(voltage) && !isNaN(current) && isNaN(resistance)) {
-    if (!negativeValueChecker(voltage, current, resistance)) {
-      result.innerHTML = "The input values has to be greater then zero!";
-      result.style.color = "red";
-      result.style.borderBottom = "1px solid red";
-    } else {
-      const res = voltage / current;
-      result.innerHTML = "Resistance: " + res.toFixed(6) + " Ω";
-      styleResult();
+  // Check for negative inputs
+  function negativeValueChecker(voltage, current, resistance) {
+    if (voltage <= 0 || current <= 0 || resistance <= 0) {
+      return false;
     }
-  } else if (!isNaN(voltage) && isNaN(current) && !isNaN(resistance)) {
-    if (!negativeValueChecker(voltage, current, resistance)) {
-      result.innerHTML = "The input values has to be greater then zero!";
-      result.style.color = "red";
-      result.style.borderBottom = "1px solid red";
-    } else {
-      const curr = voltage / resistance;
-      result.innerHTML = "Current: " + curr.toFixed(6) + " A";
-      styleResult();
-    }
-  } else if (isNaN(voltage) && !isNaN(current) && !isNaN(resistance)) {
-    if (!negativeValueChecker(voltage, current, resistance)) {
-      result.innerHTML = "The input values has to be greater then zero!";
-      result.style.color = "red";
-      result.style.borderBottom = "1px solid red";
-    } else {
-      const volt = current * resistance;
-      result.innerHTML = "Voltage: " + volt.toFixed(6) + " V";
-      styleResult();
-    }
-  } else {
-    result.innerHTML = "Please enter two values to calculate.";
-    result.style.color = "red";
-    result.style.borderBottom = "1px solid red";
+    return true;
   }
-});
 
-resetBtn.addEventListener("click", function () {
-  document.getElementById("voltage").value = "";
-  document.getElementById("current").value = "";
-  document.getElementById("resistance").value = "";
-  result.innerHTML = "";
-  resultsheading.innerHTML = "";
+  const voltageUnitsSelect = document.getElementById("voltage-units");
+  voltageUnitsSelect.addEventListener("change", calculate);
+
+  const currentUnitsSelect = document.getElementById("current-units");
+  currentUnitsSelect.addEventListener("change", calculate);
+
+  const resistanceUnitsSelect = document.getElementById("resistance-units");
+  resistanceUnitsSelect.addEventListener("change", calculate);
 });
